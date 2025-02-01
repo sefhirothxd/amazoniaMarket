824ee02aeef6f32e0eab36669a46a5c5dc0c4ecc
Bryan Vera <bveracachay@gmail.com>
2025-02-01 16:20:15 -0500

    fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,42 @@
 import { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
-import Head from 'next/head';
 
 export const metadata: Metadata = {
 	title: 'Amazonia market',
-	description: 'creado por skillien',
+	description:
+		'En AmazoniaMarket.pe, encuentra los mejores productos regionales y nacionales de Perú en nuestros minimarkets ubicados en aeropuertos. Descubre snacks, bebidas y artesanías típicas que reflejan la diversidad cultural del país. ¡Haz de tu viaje una experiencia auténtica!',
 	icons: '/favicon.png',
+	metadataBase: new URL('https://amazoniamarket.pe/'),
+	keywords: [
+		'Productos regionales Perú',
+		'Minimarket en aeropuertos Perú',
+		'Compras en aeropuertos Perú',
+		'Productos típicos peruanos',
+		'Snacks peruanos',
+		'Artesanías peruanas',
+		'Bebidas tradicionales Perú',
+		'Tienda en aeropuertos Perú',
+		'AmazoniaMarket Perú',
+		'Productos nacionales Perú',
+		'Regalos típicos peruanos',
+		'Experiencia de compra en aeropuertos',
+		'Productos auténticos de Perú',
+		'Tienda de conveniencia en aeropuertos',
+		'Compras rápidas en aeropuertos',
+	],
+	authors: [
+		{ name: 'Bryan Vera', url: 'https://bryanvera.dev/' },
+		{ name: 'Anthony Ureta', url: 'https://skillien.com/' },
+	],
+	creator: 'Skillien team',
+	publisher: 'Skillien.com',
+	alternates: {
+		canonical: '/',
+	},
+	openGraph: {
+		images: '/bannerMovil.png',
+	},
 };
 
 export default function RootLayout({
@@ -16,36 +46,6 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" suppressHydrationWarning>
-			<Head>
-				<title>Mi Página</title>
-				<meta name="description" content="Am" />
-
-				{/* Open Graph / Facebook */}
-				<meta property="og:title" content="Mi Página" />
-				<meta property="og:description" content="Am" />
-				<meta
-					property="og:image"
-					content="https://jzkbzghdxxqlugatkuqn.supabase.co/storage/v1/object/public/amazonia//ron"
-				/>
-				<meta property="og:url" content="https://tudominio.com/mi-pagina" />
-
-				{/* Twitter */}
-				<meta name="twitter:card" content="summary_large_image" />
-				<meta name="twitter:title" content="Mi Página" />
-				<meta
-					name="twitter:description"
-					content="Esta es una descripción de mi página."
-				/>
-				<meta
-					name="twitter:image"
-					content="https://jzkbzghdxxqlugatkuqn.supabase.co/storage/v1/object/public/amazonia//ron"
-				/>
-
-				{/* WhatsApp y otras plataformas */}
-				<meta property="og:image:width" content="1200" />
-				<meta property="og:image:height" content="630" />
-				<meta property="og:image:type" content="image/jpeg" />
-			</Head>
 			<body className="font-mali">
 				{children}
 				<Toaster />
